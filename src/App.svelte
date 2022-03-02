32ea75edcf26483b20b05186add49343d19c28b7
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-03-03 03:55:33 +0900

    links

--- src/App.svelte
+++ src/App.svelte
@@ -11,14 +11,14 @@
   let chart
 
   const colorRating = (rating) => {
-    if (rating < 400) return [128,128,128]
-    else if (rating < 800) return [128,64,0]
-    else if (rating < 1200) return [0,128,0]
-    else if (rating < 1600) return [0,192,192]
-    else if (rating < 2000) return [0,0,255]
-    else if (rating < 2400) return [192,192,0]
-    else if (rating < 2800) return [255,128,0]
-    return [255,0,0]
+    if (rating < 400) return [128, 128, 128]
+    else if (rating < 800) return [128, 64, 0]
+    else if (rating < 1200) return [0, 128, 0]
+    else if (rating < 1600) return [0, 192, 192]
+    else if (rating < 2000) return [0, 0, 255]
+    else if (rating < 2400) return [192, 192, 0]
+    else if (rating < 2800) return [255, 128, 0]
+    return [255, 0, 0]
   }
 
   const calculatePerformance = (newPerformance?: number) => {
@@ -73,27 +73,34 @@
       const xScale = target.scales["x-axis-0"]
       const yScale = target.scales["y-axis-0"]
 
-      for (const rate of [...Array(8).keys()].map((i) => {return i * 400})) {
-        chartCanvas.getContext("2d").fillStyle = `rgba(${colorRating(rate).join(", ")}, 0.2)`
+      for (const rate of [...Array(8).keys()].map((i) => {
+        return i * 400
+      })) {
+        chartCanvas.getContext("2d").fillStyle = `rgba(${colorRating(rate).join(
+          ", "
+        )}, 0.2)`
         chartCanvas
           .getContext("2d")
           .fillRect(
             xScale.left,
-            Math.min(yScale.getPixelForValue(rate+400),yScale.bottom),
+            Math.min(yScale.getPixelForValue(rate + 400), yScale.bottom),
             xScale.width,
-            Math.min(yScale.getPixelForValue(rate), yScale.bottom) - Math.min(yScale.getPixelForValue(rate+400), yScale.bottom)
+            Math.min(yScale.getPixelForValue(rate), yScale.bottom) -
+              Math.min(yScale.getPixelForValue(rate + 400), yScale.bottom)
           )
-        chartCanvas.getContext("2d").fillStyle = `rgba(${colorRating(rate).join(", ")}, 0.5)`
+        chartCanvas.getContext("2d").fillStyle = `rgba(${colorRating(rate).join(
+          ", "
+        )}, 0.5)`
         chartCanvas
           .getContext("2d")
           .fillRect(
             xScale.getPixelForValue(rate),
             yScale.bottom,
-            Math.min(xScale.getPixelForValue(rate+400),xScale.right) - Math.min(xScale.getPixelForValue(rate),xScale.right),
-            5,
+            Math.min(xScale.getPixelForValue(rate + 400), xScale.right) -
+              Math.min(xScale.getPixelForValue(rate), xScale.right),
+            5
           )
       }
-
     }
 
     chart = new Chart(chartCanvas, {
@@ -161,6 +168,25 @@
 
   <h2>次回のレート</h2>
   <p><canvas width={1280} height={720} bind:this={chartCanvas} /></p>
+
+  <h2>View on GitHub</h2>
+  <p>
+    <a href="https://github.com/3w36zj6/atcoder-heuristic-rating-estimator"
+      ><img
+        alt="atcoder-heuristic-rating-estimator"
+        src="https://gh-card.dev/repos/3w36zj6/atcoder-heuristic-rating-estimator.svg?fullname="
+      /></a
+    >
+  </p>
+
+  <h2>Links</h2>
+  <ul>
+    <li>
+      <a href="https://www.dropbox.com/s/ne358pdixfafppm/AHC_rating.pdf"
+        >AHC Rating System</a
+      >
+    </li>
+  </ul>
 </main>
 
 <style>
